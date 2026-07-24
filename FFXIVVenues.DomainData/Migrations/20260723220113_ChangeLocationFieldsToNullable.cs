bb9835cf685f44afe2447f5f77e9db42780ac0e6
Kana Ki <kana@ffxivvenues.com>
2026-07-24 01:29:23 +0100

    Fix error when saving venue with location override

---
+++ FFXIVVenues.DomainData/Migrations/20260723220113_ChangeLocationFieldsToNullable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FFXIVVenues.DomainData.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLocationFieldsToNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "World",
+                schema: "Venues",
+                table: "Locations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "District",
+                schema: "Venues",
+                table: "Locations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DataCenter",
+                schema: "Venues",
+                table: "Locations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "World",
+                schema: "Venues",
+                table: "Locations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "District",
+                schema: "Venues",
+                table: "Locations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DataCenter",
+                schema: "Venues",
+                table: "Locations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
