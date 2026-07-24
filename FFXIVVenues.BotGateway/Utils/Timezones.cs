580f032dcc5b5d14cb06e1ec759ac75dde1d8d01
Kana Ki <kana@ffxivvenues.com>
2026-07-24 02:11:24 +0100

    Reorgnise timezones

--- FFXIVVenues.BotGateway/Utils/Timezones.cs
+++ FFXIVVenues.BotGateway/Utils/Timezones.cs
@@ -16,11 +16,11 @@ public static class TimeZones
         ( "Europe/London",       "Greenwich Mean Time (GMT)",     "UTC+0 Standard, UTC+1 Summer" ),
         ( "Europe/Budapest",     "Central European Time (CET)",   "UTC+1 Standard, UTC+2 Summer" ),
         ( "Europe/Chisinau",     "Eastern European Time (EET)",   "UTC+2 Standard, UTC+3 Summer" ),
-        ( "Asia/Tokyo",          "Japan Standard Time (JST)",     "UTC+9" ),
+        ( "Asia/Bangkok",        "Indochina Time (ICT)",          "UTC+7" ),
         ( "Asia/Hong_Kong",      "Hong Kong Time (HKT)",          "UTC+8" ),
         ( "Asia/Singapore",      "Singapore Time (SGT)",          "UTC+8" ),
         ( "Asia/Manila",         "Philippine Time (PHT)",         "UTC+8" ),
-        ( "Asia/Bangkok",        "Indochina Time (ICT)",          "UTC+7" ),
+        ( "Asia/Tokyo",          "Japan Standard Time (JST)",     "UTC+9" ),
         ( "Australia/Perth",     "Australian Western Time (AWST)","UTC+8" ),
         ( "Australia/Adelaide",  "Australian Central Time (ACST)","UTC+9:30 Standard, UTC+10:30 Summer" ),
         ( "Australia/Sydney",    "Australian Eastern Time (AEST)","UTC+10 Standard, UTC+11 Summer" )
